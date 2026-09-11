1e7682bdb265eba841a890605fb7bc0e8d0065e0
yyn212 <yairyintnaing212@gmail.com>
2026-09-11 14:35:17 +0630

    first-commmit

---
+++ calculator.py
@@ -0,0 +1,16 @@
+num1 = 5
+num2 = 1
+
+add = num1 + num2
+
+print("sum", add)
+
+n1 = int(input("Enter num1"))
+n2 = int(input("Enter num2"))
+
+sum = n1 + n2
+sub = n1 - n2
+
+print("SUM", sum)
+print("SUB", sub)
+
