1b8493138888413c8f7ef2570eba316717ce71da
yyn212 <yairyintnaing212@gmail.com>
2026-09-11 14:37:51 +0630

    Update initial numbers and add subtraction feature
    change numbers and add sub

--- calculator.py
+++ calculator.py
@@ -1,15 +1,18 @@
-num1 = 5
-num2 = 1
+num1 = 2012
+num2 = 2003
 
 add = num1 + num2
+sub = num1 - num2
 
 print("sum", add)
+print("sub", sub)
 
 n1 = int(input("Enter num1"))
 n2 = int(input("Enter num2"))
 
 sum = n1 + n2
 sub = n1 - n2
+
 
 print("SUM", sum)
 print("SUB", sub)
